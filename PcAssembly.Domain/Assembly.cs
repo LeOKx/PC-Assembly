74c030403707ce1779a3d5ae6a120031b6d37198
Oleg <leoleg1999@gmail.com>
2022-03-28 16:05:31 +0300

    Changes in Assembly

--- PcAssembly.Domain/Assembly.cs
+++ PcAssembly.Domain/Assembly.cs
@@ -4,17 +4,12 @@ namespace PcAssembly.Domain
 {
     public class Assembly : BaseEntity
     {
-        [Required]
+        //[Required]
         public string Name { get; set; }
         public int cpuId { get; set; }
         public CPU Cpu { get; set; }
         [ConcurrencyCheck]
         public double TotalPrice { get; set; }
 
-        //public override bool Equals(object? obj)
-        //{
-        //    if (obj is Assembly assembly) return Id == assembly.Id;
-        //    return false;
-        //}
     }
 }
