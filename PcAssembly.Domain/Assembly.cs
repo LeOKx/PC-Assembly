f24b80d4a2e275754ee3c1e891bf6c513fefa231
Oleg <leoleg1999@gmail.com>
2022-03-28 16:07:10 +0300

    delete Manufacturer Info

--- PcAssembly.Domain/Assembly.cs
+++ PcAssembly.Domain/Assembly.cs
@@ -9,11 +9,5 @@ namespace PcAssembly.Domain
         public CPU Cpu { get; set; }
         [ConcurrencyCheck]
         public double TotalPrice { get; set; } = 0;
-
-        //public override bool Equals(object? obj)
-        //{
-        //    if (obj is Assembly assembly) return Id == assembly.Id;
-        //    return false;
-        //}
     }
 }
--- PcAssembly.Domain/Component.cs
+++ PcAssembly.Domain/Component.cs
@@ -1,11 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PcAssembly.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace PcAssembly.Domain
 {
     public abstract class Component : BaseEntity
     {
+        [Required]
+        [MaxLength(50)]
+        public string Model { get; set; }
+        [Required]
+        public Company Company { get; set; }
+        [Required]
+        public TypeComponent Type { get; set; }
+        [ConcurrencyCheck]
+        public double Price { get; set; } = 0;
         public int PowerConsumption { get; set; } = 100;
         [Required]
-        public ManufacturerInfo ManufacturerInfo { get; set; }
     }
 }
