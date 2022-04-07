249d36254e62593eab9be3a58874b3845f8f540c
Oleg <leoleg1999@gmail.com>
2022-04-07 17:55:21 +0300

    save

--- PcAssembly.TestPolygon/Program.cs
+++ PcAssembly.TestPolygon/Program.cs
@@ -8,7 +8,7 @@ namespace PcAssembly.TestPolygon
         static async Task Main(string[] args)
         {
             await using var dbContext = new DataContext();
-            //await MappingAdvanced.ConcurrencyHandle(dbContext);
+            await MappingAdvanced.ConcurrencyHandle(dbContext);
             await Seed.SeedCPUs(dbContext);
             await Seed.SeedGraphicCards(dbContext);
             await Queries.GetAllComponents(dbContext);
